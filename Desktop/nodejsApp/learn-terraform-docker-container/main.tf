309615cea408f1366522786d5680368cdb36a807
afratyellin <efratyellin014@gmail.com>
2023-10-31 13:48:28 +0200

    Your commit message

---
+++ Desktop/nodejsApp/learn-terraform-docker-container/main.tf
@@ -0,0 +1,36 @@
+# main.tf
+
+terraform {
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  host    = "npipe:////.//pipe//docker_engine"
+}
+
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.image_id
+  name  = "tutorial"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
+
+# variables.tf
+
+variable "container_name" {
+  description = "Value of the name for the Docker container"
+  type        = string
+  default     = "ExampleNginxContainer"
+}
